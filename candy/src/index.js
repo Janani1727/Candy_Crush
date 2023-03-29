6b1cb55b036e958e418235c7150b82c0b5632aad
janani <jayaramanjanani73@gmail.com>
2023-03-29 12:36:03 +0530

    candy crush

--- candy/src/index.js
+++ candy/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
-import reportWebVitals from './reportWebVitals';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <ChakraProvider>
        <App />
     </ChakraProvider>
-    
-  
-
-    
+     
 );
-reportWebVitals();
+
